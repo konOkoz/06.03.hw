0d364c3c985cf2f23330026c22c2362fe51a6906
konOkoz <new2pro.ware@gmail.com>
2023-03-06 17:46:25 +0100

    homework 06.03

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+    Student s1=new Student();
+    Student s2=new Student();
+    Student s3=new Student();
+    s1.name="Ivan";
+    s1.number="+4917622123";
+    s2.name="Mykhail";
+    s2.number="+1817623";
+    s3.name="John";
+    s3.number="+21239";
+    Student s4 = new Student("Jack","+94715");
+    Student s5 = new Student("Mary","+176146832");
+
+    }
+}
---
+++ src/Student.java
@@ -0,0 +1,22 @@
+public class Student {
+       /*Задача 1
+Создать класс Student c полями (переменными) имя и номер.
+Создать несколько обьектов этого класса
+
+Задача 2
+Добавить в класс Student конструктор с параметрами имя и номер.
+Создать еще несколько обьектов класса с использованием этого конструктора
+     */
+
+    String name;
+    String number;
+
+    public Student(){
+
+    }
+
+    public Student(String name, String number){
+        this.name=name;
+        this.number=number;
+    }
+}
